7a378ab9e96f82379861292c59695c62e37e246b
MagicalBrooke <44682367+MagicalBrooke@users.noreply.github.com>
2018-12-29 21:01:26 +0000

    please just add these files

---
+++ javascript/javascript.js
@@ -0,0 +1,7 @@
+<script>
+// When the user clicks on the button, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
